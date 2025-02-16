3f359c1d6710cc8ea2749a1794496902314be501
Adrian Rojek <72630248+AdrRojek@users.noreply.github.com>
2025-02-16 20:49:55 +0100

    Initial Commit

---
+++ WaterReminderTests/WaterReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaterReminderTests.swift
+//  WaterReminderTests
+//
+//  Created by adrian on 16/02/2025.
+//
+
+import Testing
+@testable import WaterReminder
+
+struct WaterReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
