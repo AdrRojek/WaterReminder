d469798078495f4733598e6db3f89decc856f83b
Adrian Rojek <72630248+AdrRojek@users.noreply.github.com>
2025-02-21 16:35:20 +0100

    size of text

--- WaterReminder/ContentView.swift
+++ WaterReminder/ContentView.swift
@@ -104,7 +104,8 @@ struct ContentView: View {
                         .foregroundColor(.white)
                         .cornerRadius(10)
                         
-                        Text("Stan bojlera \(boilerWater)")
+                        Text("Stan bojlera: \(boilerWater)")
+                            .font(.custom("FONT_NAME", size: 10))
                     }else{
                         Text("Uzupełnij bojler")
                         
